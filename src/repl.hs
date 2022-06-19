cb47d0c7bc58ce8afff6ecfed4eb253bb05ff3fa
Nelson-Jnrnd <79466777+Nelson-Jnrnd@users.noreply.github.com>
2022-06-19 11:03:02 +0200

    debut repl et eval

---
+++ src/repl.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Lexer
+import Parser
+--import Semantic
+import Eval
+import System.IO
+
+
+repl :: Env -> IO ()
+repl env =
+    do
+        putStr "> "
+        hFlush stdout
+        line <- getLine
+        let token = alexScanTokens line -- lexing
+        let ast = parser token -- parsing
+        --let (t, newEnvType) = typeof ast env -- typechecking
+        let evalReturn = eval ast env -- evaluation
+        let newEnv = 
+            case evalReturn of
+                Expr lit -> env
+                Def envEntry -> envEntry : env
+        putStr "\n"
+        repl newEnv
+
+main = repl []
